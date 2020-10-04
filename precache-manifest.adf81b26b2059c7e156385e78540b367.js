5d5bdd9131532805dd853ad264f0f9c9482dea41
sromelus <46148909+sromelus@users.noreply.github.com>
2020-10-04 16:38:44 -0400

    Updates

---
+++ precache-manifest.adf81b26b2059c7e156385e78540b367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565abecb0cb0bdf291e29b7b24d6b1d2",
+    "url": "/the-weather-today/index.html"
+  },
+  {
+    "revision": "fdd2ba39e46de2a2cf08",
+    "url": "/the-weather-today/static/css/main.43ea1669.chunk.css"
+  },
+  {
+    "revision": "ba21337b68f454f9994a",
+    "url": "/the-weather-today/static/js/2.244a1d8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/the-weather-today/static/js/2.244a1d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd2ba39e46de2a2cf08",
+    "url": "/the-weather-today/static/js/main.002b2b3e.chunk.js"
+  },
+  {
+    "revision": "6df38ee25888b9572b29",
+    "url": "/the-weather-today/static/js/runtime-main.9c87e8f4.js"
+  }
+]);
